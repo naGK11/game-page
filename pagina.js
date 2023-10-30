8cf36b3d722e19b4b57255b6ca9190ef66360992
naGK11 <131674804+naGK11@users.noreply.github.com>
2023-10-30 20:07:08 -0300

    Add files via upload

--- pagina.js
+++ pagina.js
@@ -1,16 +1,36 @@
-document.addEventListener("keyup", e=>{
+// Obtén la referencia del elemento img con la clase 'menu'
+const menuImg = document.querySelector('.menu');
 
-    e.target.matches("#srch-bar")
-    console.log(e.target.value)
-    
-    if (e.target.matches("srch-bar")){
+// Obtén la referencia del elemento con la clase 'sidebar__container'
+const sidebarContainer = document.querySelector('.sidebar__container');
 
-        document.querySelectorAll("game__container").forEach( game=>{
-
-            game.textContent.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase())
-                ?game.classList.remove("filtro")
-                :game.classList.add("fuiltro")                
-        })
+// Agrega un controlador de eventos de clic al elemento img
+menuImg.addEventListener('click', function() {
+    // Si el display del sidebar es 'none', cambia a 'flex'
+    if (sidebarContainer.style.display === 'none') {
+        sidebarContainer.style.display = 'flex';
+    } 
+    // Si el display del sidebar es 'flex', cambia a 'none'
+    else {
+        sidebarContainer.style.display = 'none';
     }
+});
 
-})
+//lo mismo que arriba pero para el menu de cuenta de arriba a la derecha
+document.querySelector('.user__btn').addEventListener('click', function(){
+    var dropMenu = document.querySelector('.drop__menu');
+    if(dropMenu.style.display === 'none'){
+        dropMenu.style.display = 'flex';
+    } else {
+        dropMenu.style.display = 'none';
+    }
+});
+
+
+
+
+
+
+
+
+
